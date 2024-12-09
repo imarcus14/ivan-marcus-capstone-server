15c8f3a2476cb597458147ad427d7c75d5326c90
imarcus14 <ivanmarcus1@gmail.com>
2024-12-09 15:12:19 -0500

    Change photo URLs

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 8081;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(process.cwd(), "uploads")));
-
+app.use('/uploads', express.static("uploads"));
 app.use("/dogs", dogsRoutes);
 app.use("/users", usersRoutes);
 
